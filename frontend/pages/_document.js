8cabc9118fd4cb103d279e307e8c3230498779ca
Andry <andry@looka.win>
2026-04-28 09:48:09 +0000

    deploy 2026-04-28 09:48 — CID: QmQRiuqYwTWWnKJrWabHx1zJkS1bzxq93d9esTqvrvrVwv

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="fr">
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c22e26a3-bf49-4ccf-9328-aafb046df577"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
